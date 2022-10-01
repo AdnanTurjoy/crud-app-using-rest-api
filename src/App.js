a40dd686387d357c8d8d7e19a6217d84eed2b5a5
AdnanTurjoy <aturjoy4@gmail.com>
2022-10-01 15:58:27 +0600

    READ(get method)

--- src/App.js
+++ src/App.js
@@ -1,23 +1,54 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from "react";
 
+import "./App.css";
+const URL = "https://rest-api-without-db.herokuapp.com/users/";
 function App() {
+  const [users, setUsers] = useState(null);
+  const [error, setError] = useState("");
+  const [isLoading, setIsLoading] = useState(true);
+  const getUser = () => {
+    fetch(URL)
+      .then((res) => {
+        if (!res.ok) {
+          throw Error("Could not fetch");
+        } else {
+          return res.json();
+        }
+      })
+      .then((data) => {
+        setUsers(data.users);
+        setIsLoading(false);
+      })
+      .catch((err) => {
+        setError(err.message);
+      })
+      .finally(() => {
+        setIsLoading(false);
+      });
+  };
+  console.log(users);
+  useEffect(() => {
+    getUser();
+  }, []);
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>User Management APP</h1>
+      {isLoading && <p>Loading...</p>}
+      {error && { error }}
+      <section>
+        {users &&
+          users.map((user) => {
+            const { id, username, email } = user;
+            return (
+              <article className="card" key={id}>
+                <p>{username}</p>
+                <p>{email}</p>
+                <button className="btn">Edit</button>
+                <button className="btn">Delete</button>
+              </article>
+            );
+          })}
+      </section>
     </div>
   );
 }
